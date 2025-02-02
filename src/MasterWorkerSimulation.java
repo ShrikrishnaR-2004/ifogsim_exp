ee3fd9f930e46716ee2fac68c5417f68219eafae
Shrikrishna <rshrikrishna84@gmail.com>
2025-02-02 18:31:35 +0530

    added new changes

--- src/MasterWorkerSimulation.java
+++ src/MasterWorkerSimulation.java
@@ -1,77 +1,77 @@
-import org.fog.application.AppModule;
-import org.fog.application.Application;
-import org.fog.entities.Actuator;
-import org.fog.entities.FogBroker;
-import org.fog.entities.FogDevice;
-import org.fog.entities.Sensor;
-import org.fog.entities.Tuple;
-import org.fog.placement.ModuleMapping;
-import org.fog.placement.ModulePlacementEdgewards;
-import org.fog.utils.FogUtils;
-import org.fog.utils.Logger;
-import org.cloudbus.cloudsim.core.CloudSim;
-
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
+
+class Fognode1 {
+    String name;
+    int processingPower; // MIPS
+    int memory; // MB
+    int storage; // MB
+    int bandwidth; // Mbps
+    List<Task> tasks;
+
+    public Fognode1(String name, int processingPower, int memory, int storage, int bandwidth) {
+        this.name = name;
+        this.processingPower = processingPower;
+        this.memory = memory;
+        this.storage = storage;
+        this.bandwidth = bandwidth;
+        this.tasks = new ArrayList<>();
+    }
+
+    public void assignTask(Task task) { // This one is for the Master to receive tasks
+        tasks.add(task);
+        System.out.println(name + " assigned " + task.name);
+    }
+
+    public void assignTask(Fognode1 worker, Task task) { // This one is for the Master to distribute
+        worker.assignTask(task); // Delegate to the worker
+        System.out.println(name + " assigned " + task.name + " to " + worker.name);
+    }
+
+    public void executeTasks() {
+        for (Task task : tasks) {
+            System.out.println(name + " executing " + task.name);
+            int executionTime = task.processingRequirement / processingPower; // Simplified execution time calculation
+            try {
+                Thread.sleep(executionTime * 1000); // Simulate execution time
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + " finished " + task.name);
+        }
+        tasks.clear(); // clear the task after execution.
+    }
+}
+
+class Task {
+    String name;
+    int processingRequirement; // MI (Million Instructions)
+
+    public Task(String name, int processingRequirement) {
+        this.name = name;
+        this.processingRequirement = processingRequirement;
+    }
+}
 
 public class MasterWorkerSimulation {
     public static void main(String[] args) {
-        Logger.ENABLED = true;
+        // Master node setup
+        Fognode1 master = new Fognode1("Master", 4000, 8192, 50000, 1000);
 
-        int numUsers = 1;
-        Calendar calendar = Calendar.getInstance();
-        CloudSim.init(numUsers, calendar, false);
+        // Worker nodes setup
+        Fognode1 worker1 = new Fognode1("Worker1", 2000, 4096, 20000, 500);
+        Fognode1 worker2 = new Fognode1("Worker2", 2000, 4096, 20000, 500);
 
-        // Create a broker to manage fog resources
-        FogBroker broker = new FogBroker("Broker");
+        // Task distribution
+        Task task1 = new Task("Task1", 500);
+        Task task2 = new Task("Task2", 700);
 
-        // Define Master and Worker Nodes
-        List<FogDevice> fogDevices = new ArrayList<>();
-        FogDevice master = createFogDevice("Master", 4000, 8192, 50000, 1000);
-        FogDevice worker1 = createFogDevice("Worker1", 2000, 4096, 20000, 500);
-        FogDevice worker2 = createFogDevice("Worker2", 2000, 4096, 20000, 500);
+        master.assignTask(task1); // Master receives tasks
+        master.assignTask(task2);
 
-        fogDevices.add(master);
-        fogDevices.add(worker1);
-        fogDevices.add(worker2);
-
-        // Create application
-        Application application = createApplication("FogApp", broker.getId());
-
-        // **Fix: Define sensors and actuators (empty for now)**
-        List<Sensor> sensors = new ArrayList<>();
-        List<Actuator> actuators = new ArrayList<>();
-
-        // **Fix: Create a ModuleMapping**
-        ModuleMapping moduleMapping = ModuleMapping.createModuleMapping();
-        moduleMapping.addModuleToDevice("MasterModule", "Master");
-        moduleMapping.addModuleToDevice("WorkerModule", "Worker1");
-        moduleMapping.addModuleToDevice("WorkerModule", "Worker2");
-
-        // **Fix: Use correct constructor**
-        new ModulePlacementEdgewards(fogDevices, sensors, actuators, application, moduleMapping);
-
-        CloudSim.startSimulation();
-        CloudSim.stopSimulation();
+        master.assignTask(worker1, task1); // Master distributes to workers
+        master.assignTask(worker2, task2);
 
         System.out.println("Simulation Completed");
-    }
-
-    private static FogDevice createFogDevice(String name, int mips, int ram, long storage, int bw) {
-        return new FogDevice(name, mips, ram, storage, bw, 0, 0, 0, 0);
-    }
-
-    private static Application createApplication(String appId, int userId) {
-        Application application = new Application(appId, userId);
-
-        // Define app modules (master & worker tasks)
-        application.addAppModule("MasterModule", 10);
-        application.addAppModule("WorkerModule", 5);
-
-        // Define tuples representing tasks
-        application.addAppEdge("MasterModule", "WorkerModule", 500, 100, "TASK", Tuple.UP, AppModule.PEER);
-
-        return application;
     }
 }
